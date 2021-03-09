6741ae8efd9762e4cd00b5926646e1eb02c54179
victorviro <victor_sanchez91@hotmail.es>
2021-03-09 20:37:59 +0100

    ref #491 Removed outdated file

--- docker/airflow/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-
-airflow db init
-airflow users create  --username victorviro --password vikone91 \
-    --firstname Victor --lastname Rodeño --role Admin --email vrodeno@ucm.es
-airflow webserver -p 8080
