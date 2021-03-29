1ec6e8e76d386f6b9dd00d55cc2d3321cffb641c
victorviro <victor_sanchez91@hotmail.es>
2021-03-29 13:57:15 +0200

    ref #552 Updated use case to get data Using interface insted inherint class in thye definition of the use case

--- src/get_data/application/get_data_use_case.py
+++ src/get_data/application/get_data_use_case.py
@@ -1,23 +1,24 @@
 import os
 
 from src.get_data.domain.data_downloander import IDataDownloander
-from src.get_data.infrastructure.json_data_saver import JSONDataSaver
+from src.shared.interfaces.data_file_saver import IDataFileSaver
 
 
 class GetData:
     """
     Class to download the data in some way by calling the method
     `download_data` of object IDataDownloander and store it by calling
-    the method `save_data` of object JSONDataSaver
+    the method `save_data` of object IDataFileSaver
 
     :param data_downloander: Object with a method to download data
     :type data_downloander: IDataDownloander
-    :param data_saver: Object with a method to save data
-    :type data_saver: JSONDataSaver
+    :param data_file_saver: Object with a method to save data
+    :type data_file_saver: IDataFileSaver
     """
-    def __init__(self, data_downloander: IDataDownloander, data_saver: JSONDataSaver):
+    def __init__(self, data_downloander: IDataDownloander,
+                 data_file_saver: IDataFileSaver):
         self.data_downloander = data_downloander
-        self.data_saver = data_saver
+        self.data_file_saver = data_file_saver
 
     def execute(self, file_path: str, data_api_url: str):
         # ping
@@ -26,14 +27,14 @@ class GetData:
             raise Exception(f'Data downloaded is not a dict. It is a {type(data)}')
         if not os.path.exists(os.path.dirname(file_path)):
             raise Exception(f'Path {os.path.dirname(file_path)} does not exist')
-        self.data_saver.save_data(file_path=file_path, data=data)
+        self.data_file_saver.save_data(file_path=file_path, data=data)
 
     @staticmethod
     def build(data_downloander: IDataDownloander,
-              data_saver: JSONDataSaver):
+              data_file_saver: IDataFileSaver):
 
         get_data = GetData(
             data_downloander=data_downloander,
-            data_saver=data_saver
+            data_file_saver=data_file_saver
         )
         return get_data
