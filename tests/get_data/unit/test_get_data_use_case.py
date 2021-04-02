0a63743c59ce27123472ac32f9f4038c2738a37f
victorviro <victor_sanchez91@hotmail.es>
2021-04-02 10:23:43 +0200

    ref #553 Updated unit tests in step for downloading the data

--- tests/get_data/unit/test_get_data_use_case.py
+++ tests/get_data/unit/test_get_data_use_case.py
@@ -16,35 +16,48 @@ def test_get_data_use_case_should_complete_process_returning_success():
     mock_data_file_saver = Mock(IDataFileSaver)
     mock_data_file_saver.save_data = Mock()
 
-    use_case = GetData(
+    use_case = GetData.build(
         data_downloander=mock_data_downloander,
-        data_file_saver=mock_data_file_saver,
+        data_file_saver=mock_data_file_saver
     )
-    result = use_case.execute(
-        file_path=getcwd(),
-        data_api_url="api url"
-    )
+    result = use_case.execute(file_path=getcwd())
 
     mock_data_downloander.download_data.assert_called_once()
     mock_data_file_saver.save_data.assert_called_once()
 
 
 @pytest.mark.unit
-def test_get_data_use_case_should_raise_exception_due_non_exist_file_path():
+def test_get_data_use_case_should_raise_exception_due_non_exist_data_path():
     mock_data_downloander = Mock(IDataDownloander)
     mock_data_downloander.download_data = Mock()
 
     mock_data_file_saver = Mock(IDataFileSaver)
 
-    use_case = GetData(
+    use_case = GetData.build(
         data_downloander=mock_data_downloander,
-        data_file_saver=mock_data_file_saver,
+        data_file_saver=mock_data_file_saver
     )
     with pytest.raises(Exception):
-        result = use_case.execute(
-            file_path="non path",
-            data_api_url="api url"
-        )
+        result = use_case.execute(file_path="no_path/no_file")
 
     mock_data_downloander.download_data.assert_called_once()
     mock_data_file_saver.save_data.assert_not_called()
+
+
+@pytest.mark.unit
+def test_get_data_use_case_should_raise_exception_due_non_exist_data_file_path():
+    mock_data_downloander = Mock(IDataDownloander)
+    mock_data_downloander.download_data = Mock()
+
+    mock_data_file_saver = Mock(IDataFileSaver)
+    mock_data_file_saver.save_data = Mock()
+
+    use_case = GetData.build(
+        data_downloander=mock_data_downloander,
+        data_file_saver=mock_data_file_saver
+    )
+    with pytest.raises(Exception):
+        result = use_case.execute(file_path=f"{getcwd()}/no_file")
+
+    mock_data_downloander.download_data.assert_called_once()
+    mock_data_file_saver.save_data.assert_called_once()
