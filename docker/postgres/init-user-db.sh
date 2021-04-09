e5ad6b38d4169078aae471f4245d5a1e2456d014
victorviro <victor_sanchez91@hotmail.es>
2021-04-09 12:50:36 +0200

    ref #492 Updated config of container for postgres. Updated docker-compose-dev file (added env variables)

--- docker/postgres/init-user-db.sh
+++ docker/postgres/init-user-db.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-#--dbname "$POSTGRES_DB"
+
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     
     CREATE DATABASE "$POSTGRES_MLFLOW_DB";
