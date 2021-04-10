41fe9215aee69b6fce379107c72023325dbc5395
victorviro <victor_sanchez91@hotmail.es>
2021-04-10 10:52:30 +0200

    ref #553 Updated unit tests For version data use case

--- tests/version_data/unit/test_version_data_use_case.py
+++ tests/version_data/unit/test_version_data_use_case.py
@@ -14,14 +14,12 @@ def test_version_data_use_case_should_complete_process_returning_success():
     mock_data_versioner.version_data = Mock(return_value={})
 
     mock_data_tracker = Mock(IDataTracker)
-    mock_data_tracker.track_data = Mock()
 
     use_case = VersionTrackData.build(data_versioner=mock_data_versioner,
                                       data_tracker=mock_data_tracker)
     result = use_case.execute(data_file_path=getcwd(), data_version=0)
 
     mock_data_versioner.version_data.assert_called_once()
-    mock_data_tracker.track_data.assert_called_once()
 
 
 @pytest.mark.unit
@@ -36,4 +34,3 @@ def test_version_data_use_case_should_raise_exception_due_non_exist_data_file_pa
         result = use_case.execute(file_path="no_file")
 
     mock_data_versioner.version_data.assert_not_called()
-    mock_data_tracker.track_data.assert_not_called()
