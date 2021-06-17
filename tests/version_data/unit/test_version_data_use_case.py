66fbc4ea261e048f915641e223d9a47c573c3af7
victorviro <victor_sanchez91@hotmail.es>
2021-06-17 16:48:47 +0200

    ✅  ref #661 Update unit tests for use case which versions the dataset

--- tests/version_data/unit/test_version_data_use_case.py
+++ tests/version_data/unit/test_version_data_use_case.py
@@ -14,14 +14,14 @@ def test_version_data_use_case_should_complete_process_returning_success():
     mock_data_versioner.version_data = Mock(return_value={})
 
     mock_data_tracker = Mock(IDataTracker)
-    mock_data_tracker.track_items = Mock()
+    mock_data_tracker.track_information = Mock()
 
     use_case = VersionTrackData.build(data_versioner=mock_data_versioner,
                                       data_tracker=mock_data_tracker)
     use_case.execute(data_file_path=getcwd(), data_version=0)
 
     mock_data_versioner.version_data.assert_called_once()
-    mock_data_tracker.track_items.assert_called_once()
+    mock_data_tracker.track_information.assert_called_once()
 
 
 @pytest.mark.unit
@@ -29,7 +29,7 @@ def test_version_data_use_case_should_raise_exception_due_non_exist_data_file_pa
     mock_data_versioner = Mock(IDataVersioner)
     mock_data_versioner.version_data = Mock(return_value={})
     mock_data_tracker = Mock(IDataTracker)
-    mock_data_tracker.track_items = Mock()
+    mock_data_tracker.track_information = Mock()
 
     use_case = VersionTrackData.build(data_versioner=mock_data_versioner,
                                       data_tracker=mock_data_tracker)
@@ -38,4 +38,4 @@ def test_version_data_use_case_should_raise_exception_due_non_exist_data_file_pa
         use_case.execute(file_path="no_file")
 
     mock_data_versioner.version_data.assert_not_called()
-    mock_data_tracker.track_items.assert_not_called()
+    mock_data_tracker.track_information.assert_not_called()
